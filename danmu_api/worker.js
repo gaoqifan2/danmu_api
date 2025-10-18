a69a099fd6c94de0563b7f47af94e42915f0fa35
gaoqifan2 <124584068+gaoqifan2@users.noreply.github.com>
2025-10-19 00:14:58 +0800

    Update worker.js

--- danmu_api/worker.js
+++ danmu_api/worker.js
@@ -43,7 +43,7 @@ function resolveOtherServer(env) {
   return DEFAULT_OTHER_SERVER;
 }
 
-const DEFAULT_VOD_SERVERS = "金蝉@https://zy.jinchancaiji.com，vod@https://www.caiji.cyou"; // 默认 vod站点配置，格式：名称@URL,名称@URL
+const DEFAULT_VOD_SERVERS = "金蝉@https://zy.jinchancaiji.com,vod@https://www.caiji.cyou"; // 默认 vod站点配置，格式：名称@URL,名称@URL
 let vodServers = [];
 
 function resolveVodServers(env) {
@@ -104,7 +104,7 @@ function resolveYoukuConcurrency(env) {
   return Math.min(DEFAULT_YOUKU_CONCURRENCY, 16);
 }
 
-const DEFAULT_SOURCE_ORDER = "金蝉,tencent,vod,renren,hanjutv"; // 默认 源排序（不包含tencent，需要时请通过SOURCE_ORDER环境变量添加）
+const DEFAULT_SOURCE_ORDER = "金蝉,tencent,vod,360，renren,hanjutv"; // 默认 源排序（不包含tencent，需要时请通过SOURCE_ORDER环境变量添加）
 let sourceOrderArr = [];
 
 function resolveSourceOrder(env, deployPlatform) {
